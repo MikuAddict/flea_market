6b9971418cbe80db7fac68a3e7df30fb4e39a657
MikuLTF <gumingdebaka@gmali.com>
2025-12-07 17:23:42 +0800

    refactor: 调整错误码状态值为标准HTTP状态码，优化错误码映射关系

--- src/main/java/com/zhp/flea_market/common/ErrorCode.java
+++ src/main/java/com/zhp/flea_market/common/ErrorCode.java
@@ -5,16 +5,16 @@ package com.zhp.flea_market.common;
  */
 public enum ErrorCode {
 
-    SUCCESS(0, "ok"),
-    PARAMS_ERROR(40000, "请求参数错误"),
-    NOT_LOGIN_ERROR(40100, "未登录"),
-    NO_AUTH_ERROR(40101, "无权限"),
-    NOT_FOUND_ERROR(40400, "请求数据不存在"),
-    FORBIDDEN_ERROR(40300, "禁止访问"),
-    SYSTEM_ERROR(50000, "系统内部异常"),
-    OPERATION_ERROR(50001, "操作失败"),
-    WORD_FORBIDDEN_ERROR(422200, "包含违禁词，多次违禁将封禁账号"),
-    USER_BALANCE_NOT_ENOUGH(50002,"用户余额不足，无法调用 AI");
+    SUCCESS(200, "ok"),
+    PARAMS_ERROR(400, "请求参数错误"),
+    NOT_LOGIN_ERROR(401, "未登录"),
+    NO_AUTH_ERROR(403, "无权限"),
+    NOT_FOUND_ERROR(404, "请求数据不存在"),
+    FORBIDDEN_ERROR(405, "禁止访问"),
+    SYSTEM_ERROR(500, "系统内部异常"),
+    OPERATION_ERROR(501, "操作失败"),
+    WORD_FORBIDDEN_ERROR(422, "包含违禁词，多次违禁将封禁账号"),
+    USER_BALANCE_NOT_ENOUGH(402, "用户余额不足，无法调用 AI");
 
     /**
      * 状态码
